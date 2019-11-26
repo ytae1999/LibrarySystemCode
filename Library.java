1b4f2a962c9bc7187221574f0862a0a29ffe353b
rlgh565 <rlgh565@gmail.com>
2019-11-26 14:20:10 +0900

    도서관 메소드 수정

--- Library.java
+++ Library.java
@@ -21,35 +21,27 @@ public class Library
 
     public void RegisterOneBorrower(String name){
         Borrower borrower = new Borrower(name);
-        test
     }
 
     public void RegisterOneBook(String title, String author, int CatalogueNumber){
-<<<<<<< HEAD
-<<<<<<< HEAD
         Book book = new Book(title, author, CatalogueNumber);
-        f
 
-=======
         if(CheckBook(CatalogNumber)){
-=======
-        if(Check(CatalogueNumber)){
->>>>>>> 8fcc2c750b2d23c7c7ff1c2abe63c21344ca2ea5
-            Book book = new Book(title, author, CatalogueNumber);
-            boolean result = registeredBook.add(book);
-            if(result == true){
-                System.out.println("등록되었습니다.");
-            }
-            else{
-                System.out.println("등록에 실패하였습니다.");
+            if(Check(CatalogueNumber)){
+                Book book = new Book(title, author, CatalogueNumber);
+                boolean result = registeredBook.add(book);
+                if(result == true){
+                    System.out.println("등록되었습니다.");
+                }
+                else{
+                    System.out.println("등록에 실패하였습니다.");
+                }
             }
         }
->>>>>>> 9b8f3a166987f95a4582ea804ff74757ec4e7206
     }
-
     public void LendOneBook(String name, int CatalogueNumber){
 
-    }
+    } 
 
     public void ReturnOneBook(int CatalogueNumber){
 
