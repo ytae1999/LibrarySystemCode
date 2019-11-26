afa5acb301a0a39c473197c6b9e22e069f2d038c
rlgh565 <rlgh565@gmail.com>
2019-11-26 13:33:57 +0900

    test

--- Loan.java
+++ Loan.java
@@ -9,13 +9,18 @@ public class Loan
 {
     private int loanDate;
     private int returnDate;
-    public Loan(int loanDate, int returnDate){
+    private Book book;
+    private Borrower borrower;
+    public Loan(int loanDate, int returnDate,Book book,Borrower borrower){
         this.loanDate = loanDate;
         this.returnDate = returnDate;
+        this.book = book;
+        this.borrower = borrower;
     }
     public String toString(){
         return "Laon(" + loanDate + "," + returnDate + ")";
     }
     public void delLoan(Loan loan){
+        
     }
 }
