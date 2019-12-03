14e2832ce0f19e2de52df2d63302f7ddf100fdf2
songjuho <sojh0410@naver.com>
2019-12-03 17:46:40 +0900

    test

--- Library.java
+++ Library.java
@@ -21,10 +21,7 @@ public class Library
 
     public void RegisterOneBook(String title, String author, int CatalogueNumber){
         Book book = new Book(title, author, CatalogueNumber);
-<<<<<<< HEAD
-        
-=======
->>>>>>> 28df8275687b8e8fc602a75e61a4a525144c94db
+
     }
 
     public void LendOneBook(String name, int CatalogueNumber){
@@ -34,31 +31,25 @@ public class Library
     public void ReturnOneBook(int CatalogueNumber){
 
     }
-    
+
     public void DisplayBooksAvailableForLoan(Book book){
-<<<<<<< HEAD
-      
-=======
+
     }
 
     public boolean Check(int CatalogueNumber,TreeSet<Book> Books){
         return true;
     }
-        
+
     public boolean Check(String name,HashSet<Borrower> registeredBorrowers){
         return true;
     }
 
     public boolean Check(int CatalogueNumber,TreeSet<Book> Books){
         return true;
->>>>>>> 28df8275687b8e8fc602a75e61a4a525144c94db
+
     }
 
     public void DisplayBooksAvailableOnLoan(Book book){
-<<<<<<< HEAD
-    }
-    
-=======
         Iterator it = Book.iterator();
         while(it.hasNext() == false){
             Book book = (Book)it.next();
@@ -76,17 +67,8 @@ public class Library
         }
     }
 
->>>>>>> 28df8275687b8e8fc602a75e61a4a525144c94db
     public String DisplayBorrowersLoanBook(int CatalogueNumber)
     {
-<<<<<<< HEAD
-        
-    }
-    
-    
-}
-=======
 
-    } 
+    }
 }
->>>>>>> 28df8275687b8e8fc602a75e61a4a525144c94db
