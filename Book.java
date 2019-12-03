28df8275687b8e8fc602a75e61a4a525144c94db
태영준 <ytae1999@naver.com>
2019-12-03 17:42:06 +0900

    merge

--- Book.java
+++ Book.java
@@ -1,16 +1,7 @@
 import java.util.TreeSet;
 /**
  * Write a description of class Book here.
- *
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @author (2018315033태영준,2018210083노승욱, 2018210025현기호, 2018210059송주호)
-=======
- * @author (2018315033 태영준, 2018210083 노승욱, 2018210025 현기호, 2018210059 송주호)
->>>>>>> e9e3ad21fccbe4032e74871f1a864f33711564d2
-=======
- * @author (2018315033 태영준, 2018210083 노승욱, 2018210025 현기호, 2018210059 송주호)
->>>>>>> 4a3442b853ffa42d7a29054f46448d0c68d73bf5
  * @version (2019.11.29)
  */
 public class Book
@@ -25,41 +16,23 @@ public class Book
         this.title = title;
         this.author = author;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public Book getBook(int CatalogueNumber){
-        
+
     }
-=======
 
     public Book getBook(int CatalogueNumber){
         return Book; 
     }
 
->>>>>>> e9e3ad21fccbe4032e74871f1a864f33711564d2
-=======
-
     public Book getBook(int CatalogueNumber){
         return Book; 
     }
 
->>>>>>> 4a3442b853ffa42d7a29054f46448d0c68d73bf5
     public void delLoan(){
         this.loan = null;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public void display(Book book){
-        
-=======
-
-    public void display(){
-        this.
->>>>>>> e9e3ad21fccbe4032e74871f1a864f33711564d2
-=======
-
-    public void display(){
-        
->>>>>>> 4a3442b853ffa42d7a29054f46448d0c68d73bf5
     }
 }
